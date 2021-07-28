8001cd7a31407ad57b48d08c2ffb0d9a0692cda9
awaisspk <iamawaisiqball@gmail.com>
2021-07-28 20:45:26 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-// import Calculator from './components/Calculator.js';
-/* import Login from "./components/Login.js";
-import Mailbox from "./components/Mailbox.js";
-import ListItems from "./components/ListItems.js";
-import FlavorForm from "./components/Select.js"; */
-// import WelcomeDialog from './components/WelcomeDialog.js';
-// import NameForm from './components/NameForm.js';
-// import EssayForm from './components/EssayForm.js';
-// import FlavorForm from './components/FlavorForm.js';
-// import Reservation from './components/Reservation';
-import Calculator from './components/LiftState/Calculator';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState(['todo1', 'todo2', 'todo3']);
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <>
+      <TodoCount todos={todos} />
+      <TodoList todos={todos} />
+      <AddTodo setTodos={setTodos} />
+    </>
   );
 }
 
-export default App;
+function TodoCount({ todos }) {
+  return <div>Total Todos:{todos.length} </div>;
+}
+
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+}
+
+function AddTodo({ setTodos }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const todo = event.target.elements.todo.value;
+    if (todo !== null || undefined || '') {
+      setTodos((prevTodo) => [...prevTodo, todo]);
+    } else {
+      event.target.reset();
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" id="todo" />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+}
